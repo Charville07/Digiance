6c6b1178551715545099b12b2dd9c37de34215da
Charville Ubagan <charvilleubagan@gmail.com>
2024-01-17 21:47:45 +0800

    Configured Icon and Splash

--- android/app/src/main/java/com/digiance/MainActivity.java
+++ android/app/src/main/java/com/digiance/MainActivity.java
@@ -1,12 +1,18 @@
 package com.digiance;
 
+import android.os.Bundle; 
 import com.reactnativenavigation.NavigationActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
 public class MainActivity extends NavigationActivity {
 
-  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
+  }
 
   
 
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { Navigation } from "react-native-navigation";
+import SplashScreen from 'react-native-splash-screen'
 import App from './App';
 
 
 Navigation.registerComponent('com.myApp.WelcomeScreen', () => App);
 Navigation.events().registerAppLaunchedListener(() => {
+    SplashScreen.hide();
     Navigation.setRoot({
         root: {
             stack: {
